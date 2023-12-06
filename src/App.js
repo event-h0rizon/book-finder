0913220b10d23d6bc4ac75fe5e90edb9e8303bf5
Karthik P <141917943+event-h0rizon@users.noreply.github.com>
2023-12-06 21:19:31 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import BookResults from "./BookResults";
 import Navbar from "./Navbar";
 import SearchBook from "./SearchBook";
-import React, {useState, useReducer, useEffect, useMemo, useRef, useCallback} from "react";
+import React, { useState, useReducer, useEffect, useMemo, useRef, useCallback } from "react";
 import axios from "axios";
 import Spinner from "./Spinner";
 
@@ -9,31 +9,37 @@ import Spinner from "./Spinner";
 
 function App() {
 
-  const [query, setQuery]= useState('');
-  const [pageNumber, setPageNumber]= useState(1);
-  const [loading, setLoading]= useState(true);
-  const [error, setError]= useState(1);
-  const [hasMore, setHasMore]= useState(false);
-  const [books, setBooks]= useState([]);
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(1);
+  const [hasMore, setHasMore] = useState(false);
+  const [books, setBooks] = useState([]);
 
-  const observer= useRef()
-  const lastBookElementRef= useCallback(node=>{
-    console.log(node)
-    
-  })
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prev => prev + 1)
+      }
+    })
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore])
 
 
-  // console.log(query)
 
-
-  const handleChange= (e)=> {
+  const handleChange = (e) => {
     setQuery(e.target.value)
   }
 
   useEffect(() => {
     setLoading(true)
     setError(false)
+    if(pageNumber===1){
     setBooks([])
+    }
   }, [query, pageNumber]);
 
   useEffect(() => {
@@ -41,41 +47,58 @@ function App() {
     setError(false)
     let cancel
     axios({
-    method: 'GET',
-    url: 'https://openlibrary.org/search.json',
-    params: {q: query, page: pageNumber},
-    cancelToken: new axios.CancelToken(c => cancel= c)
-  }).then(res => {
-    console.log(res.data)
-    setBooks(prevBooks => [...prevBooks, ...res.data.docs.map(b => b.title,)])
-    setHasMore(res.data.docs.length>0)
-    setLoading(false)
-    console.log(books)
-  }).catch(e => {
-    if(axios.isCancel(e)) return
-  })
-    // Code to run after the component renders or when specific dependencies change
-
-    // This is where you can perform side effects
-    // For example, you can make API requests or manipulate the DOM
-    
-    // You can also return a cleanup function (optional)
+      method: 'GET',
+      url: 'https://openlibrary.org/search.json',
+      params: { q: query, page: pageNumber },
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      console.log(res.data)
+      setBooks(prevBooks => {
+        return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])]
+      })
+      setHasMore(res.data.docs.length > 0)
+      setLoading(false)
+      console.log(books)
+    }).catch(e => {
+      if (axios.isCancel(e)) return
+    })
     return () => cancel()
   }, [query, pageNumber]);
 
 
-
-
-   
   return (
-   <>
-    <Navbar/>
-    <h5 className="container my-3">Find books, by typing the title below</h5>
-    <SearchBook query={query} handleChange={handleChange}/>
-    {loading && <Spinner/>}
-    <BookResults books={books} observer={observer} lastBookElementRef={lastBookElementRef}/>
-   </>
-  
+    <>
+      <Navbar />
+      <h5 className="container my-3">Find books, by typing the title below</h5>
+      <SearchBook query={query} handleChange={handleChange} />
+     
+      {/* <BookResults books={books} observer={observer} lastBookElementRef={lastBookElementRef} /> */}
+      <div className='container my-3'>
+        {books.map((book, index) => {
+          if (books.length === index + 1) {
+            return <div ref={lastBookElementRef} key={index}>{book}</div>
+
+          }
+          else {
+            return <div key={index}>{book}</div>
+
+          }
+
+        })}
+        <div className="py-4">
+        {loading && <Spinner />}
+        </div>
+       
+         {/* <div>
+         {loading && 'Loading...'}
+         </div> */}
+
+
+
+
+      </div>
+    </>
+
   );
 }
 
--- src/BookResults.js
+++ src/BookResults.js
@@ -1,18 +1,23 @@
 // import React from 'react'
-import React, {useState, useReducer, useEffect, useMemo, useRef, useCallback} from "react";
+import React, { useState, useReducer, useEffect, useMemo, useRef, useCallback } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Spinner from "./Spinner";
 
 
 export default function BookResults(props) {
   return (
     <div className='container my-3'>
+
+
       {props.books.map((book, index) => {
-        if (props.books.id === index + 1) {
-          return <div ref={props.lastBookElementRef} key={book}>{book}</div>
-        }
-        else {
-          return <div key={book}>{book}</div>
-        }
+
+        return <div key={index}>{book}</div>
+
+
       })}
+
+
+
     </div>
   )
 }
